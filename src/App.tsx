83bce982cce372c2cee136fa3cb9b7117030f4b3
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-10-29 22:09:57 +0100

    language translation in SPA, ENG and CAT implemented

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,18 @@ import {
 import Chart from './components/Chart'
 import { useState } from 'react'
 import { use } from 'echarts'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+const locales = {
+    spa: { src: '../public/spanish.png', alt: 'Spanish'},
+    eng: { src: '../public/english.png', alt: 'English'},
+    cat: { src: '../public/catalan.png', alt: 'Catalan'}
+};
 
 function App() {
+    const { t, i18n } = useTranslation();
+
     let today = new Date()
     let day = today.getDay()
     let thisDay = today.getDate() - day + (day == 0 ? -6 : 1)
@@ -27,14 +37,6 @@ function App() {
     const weekExpenses = useAppSelector(selectExpenses)
     const currentMonday = new Date(getWeekBounds(week)['monday'].setHours(0,0,0,0))
     const currentSunday = new Date(getWeekBounds(week)['sunday'].setHours(23,59,59,999))
-    // console.log("weekExpenses: ", Object.entries(weekExpenses).filter(([key, value]) => {
-    //     const date = key.split('/').reverse().join('-')
-    //     return (
-    //         new Date(date) >= currentMonday &&
-    //         new Date(date) <= currentSunday
-    //         )
-    //     }))
-        //! TO BE FIXED: Monday is not included in the weekArr
 
         const weekArr = Object.entries(weekExpenses)
         .filter(([key, value]) => {
@@ -59,10 +61,14 @@ function App() {
             setWeek(nextWeek)
         }
     }
+    console.log(
+        "obj test: ", 
+        Object.entries(locales).map(([key, value]) => ({ src: value.src, alt: value.alt }))
+    );
 
     return (
         <AppLayout>
-            <nav className="flex h-12 w-80 justify-end gap-2">
+            {/* <nav className="flex h-12 w-80 justify-end gap-2">
                 <img
                     src="../public/spanish.png"
                     alt="spanish"
@@ -78,12 +84,23 @@ function App() {
                     alt="catalan"
                     className="transition-scale h-auto w-12 scale-[1] rounded-full border-2 border-custom-cream outline outline-custom-coral duration-[.2s] ease-in-out hover:scale-[1.1]"
                 />
+            </nav> */}
+            <nav className="flex h-12 w-80 justify-end gap-2">
+                {Object.entries(locales).map(([key, value]) => (
+                    <img
+                    key={value.alt}
+                    src={value.src}
+                    alt={value.alt}
+                    onClick={() => i18n.changeLanguage(key)}
+                    className="transition-scale h-auto w-12 scale-[1] rounded-full border-2 border-custom-cream outline outline-custom-coral duration-[.2s] ease-in-out hover:scale-[1.1]"
+                    />
+                ))}
             </nav>
             <div className="flex h-20 w-80 items-center rounded-xl bg-custom-coral text-white">
                 <div className="flex h-3/5 w-full pl-4">
                     <div className="flex h-full w-full flex-1 flex-col">
                         <small className="flex h-full w-full items-center text-xs">
-                            Balance total
+                            {t('app.totalBalance')}
                         </small>
                         <h3 className="flex h-full w-full items-center justify-start">
                             {count} €
@@ -112,14 +129,14 @@ function App() {
                 </div>
             </div>
             <main className="flex h-auto w-80 flex-col gap-2 rounded-xl bg-slate-100 p-4">
-                <h3>Gastos - última semana</h3>
+                <h3>{t('app.weekLatestExpenses')}</h3>
                 <div className="h-auto w-full border">
                     <Chart weekArr={weekArr} />
                 </div>
                 <div className="flex h-3/5 w-full items-center justify-center">
                     <div className="flex h-3/5 w-full flex-1 flex-col items-center justify-center">
                         <small className="flex h-full w-full items-center text-xs text-slate-400">
-                            Gastos de hoy
+                        {t('app.todayExpenses')}
                         </small>
                         <h3 className="flex h-full w-full items-center justify-start">
                             {todayCount} €
@@ -127,7 +144,7 @@ function App() {
                     </div>
                     <div className="flex-3 flex flex-col items-center justify-center text-xs">
                         <div className="w-full text-right">{diff}%</div>
-                        <div>Respecto a ayer</div>
+                        <div>{t('app.yesterdayDifference')}</div>
                     </div>
                 </div>
             </main>
@@ -135,4 +152,10 @@ function App() {
     )
 }
 
-export default App
+export default function WrapperApp() {
+    return (
+        <Suspense fallback="loading">
+            <App />
+        </Suspense>
+    )
+}
